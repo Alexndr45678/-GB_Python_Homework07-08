f9a66035cf3857bbea8c35588fa681bc30e7df49
Alexandr <111227400+Alexndr45678@users.noreply.github.com>
2022-11-24 13:06:40 +0300

    123

--- view.py
+++ view.py
@@ -16,12 +16,12 @@ def print_data(a: dict):
     pass
 
 
-#     is_num = False
-#     while not is_num:
-#         try:
-#             num = float(input('Введите число: '))
-#             if num:
-#                 is_num = True
-#         except ValueError:
-#             print('Нужно ввести число\n')
-#     return abs(num)
+    is_num = False
+    while not is_num:
+        try:
+            num = float(input('Введите число: '))
+            if num:
+                is_num = True
+        except ValueError:
+            print('Нужно ввести число\n')
+    return abs(num)
