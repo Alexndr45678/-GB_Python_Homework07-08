888d4f0c604ebfaffd28e5bf977f3fcbc865c7bf
Alexandr <111227400+Alexndr45678@users.noreply.github.com>
2022-11-24 21:24:30 +0300

    log 2.0

--- log.py
+++ log.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 def result_log(dct: dict):
     time = datetime.now().strftime('%H:%M:%S')
-    with open('log.csv', 'a') as file:
+    with open('log.txt', 'a') as file:
         file.write('{}{}'
                    .format(time, dct['result']))
