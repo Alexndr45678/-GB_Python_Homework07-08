a7f4383de10fd95ff09911d8b201681a7ad9dc80
Alex Ko <help.mail015@gmail.com>
2022-11-24 18:08:59 +0200

    fix read data

--- view.py
+++ view.py
@@ -1,36 +1,34 @@
-# def read_data() -> dict:
+def read_data() -> dict:
+    print("Приветствуем Вас в калькуляторе Python")
 
-print ("Приветствуем Вас в калькуляторе Python")
+    FirstNum = float(input("Введите первое число: "))
+    SecondNum = float(input("Введите второе число: "))
 
-FirstNum = float (input("Введите первое число: "))
-SecondNum = float (input("Введите второе число: "))
-
-MathAction = input('''Выберите действие из приведенных ниже для выполнения действия:
-+ для сложения
-- Для вычитания
-* Для умножения
-/ Для деления
-^ Для возведения в квадрат \n''')
-
-def CheckOperator(MathAction):
     operator = False
     while not operator:
         try:
-            operator = str(('MathAction'))
-            if operator == '^' or '*' or '/' or '^' or '+' or '-':
+            MathAction = input('''Выберите действие из приведенных ниже для выполнения действия:
+                + для сложения
+                - Для вычитания
+                * Для умножения
+                / Для деления
+                ^ Для возведения в квадрат \n''')
+            if MathAction == '^' or '*' or '/' or '+' or '-':
                 operator = True
+            else:
+                print('Нужно ввести корректный оператор\n')
         except ValueError:
             print('Нужно ввести корректный оператор\n')
-    return abs(operator)
 
-dct = {
-    'num1': '',
-    'num2': '',
-    'operator': '',
-    'result': '',
-    'isComplex1': True,
-    'isComplex2': True,
-}
-dct ['num1'] = FirstNum
-dct ['num2'] = SecondNum
-dct ['operator'] = MathAction
+    dct = {
+        'num1': '',
+        'num2': '',
+        'operator': '',
+        'result': '',
+    }
+
+    dct['num1'] = FirstNum
+    dct['num2'] = SecondNum
+    dct['operator'] = MathAction
+
+    return dct
