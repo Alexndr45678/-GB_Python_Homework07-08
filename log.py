e4d051dbf4dad035d1d19d1d44d0f1fb037e67ab
Alex Ko <help.mail015@gmail.com>
2022-11-26 03:46:21 +0200

    end v3.1

--- log.py
+++ log.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 def result_log(dct: dict):
     time = datetime.now().strftime('%H:%M:%S')
-    with open('calculator.log', 'a') as file:
+    with open('calculator.log', 'a', encoding='utf-8') as file:
         file.write('{} ==> {} {} {} = {}\n'
                    .format(time, dct['num1'], dct['operator'], dct['num2'], dct['result']))
